d70573658c56c44ef0fff8f9a19a381b0601b906
sai rohit <msrohit1919@gmail.com>
2022-07-05 12:13:29 +0530

    Added url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // https://youtu.be/jf8HVUFPYHA
   title = 'clipboard-manager';
 }
